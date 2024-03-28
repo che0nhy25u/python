c845427efb8805d2c0831675fc400e3abed6c8ba
che0nhy25u <164131554+che0nhy25u@users.noreply.github.com>
2024-03-28 15:47:18 +0900

    Create 240328_finding_tag_pyth.py

---
+++ 240328_finding_tag/240328_finding_tag_pyth.py
@@ -0,0 +1,10 @@
+with open('./test.html','r',encoding='utf-8') as f :
+    html = f.read()
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(html,'lxml')
+
+#1. 태그 이름으로 찾는 거 
+print(soup.title)
+print(soup.title.text)
